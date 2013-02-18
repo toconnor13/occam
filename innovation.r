020072a114e5ff915b18fe1bcf30b9e0d0ef2962
Tony O'Connor <toconnor13@gmail.com>
2013-02-18 16:06:49 +0000

    function to extract betas from time series regression added

--- innovation.r
+++ innovation.r
@@ -10,6 +10,7 @@ library("foreign")
 library("AER")
 
 factors <- read.dta("innovation_factors.dta")
+candidate_factors <- names(factors)[6:17]
 N <- length(factors$vc_returns)
 factors$vc_returns_l1 <- c(NA, factors$vc_returns[1:(N-1)])
 
@@ -17,7 +18,7 @@ pfs_25 = names(factors)[18:42]
 ind_10 = names(factors)[43:54]
 mom_12 = names(factors)[55:64]
 
-ts(factors, start=1927, end=2011, frequency=1)
+# ts(factors, start=1927, end=2011, frequency=1)
 
 
 capm_model <- sl ~ mktrf
@@ -34,7 +35,7 @@ add_ind_var <- function(new_var, model){
 	update(model, new)
 }
 
-ts_analysis <- function(portfolios, formula, factor=NULL){
+ts_analysis <- function(portfolios=pfs_25, formula=hml_model, factor=NULL){
 	this_formula <- formula
 	if(!missing(factor)){
 	this_formula <- add_ind_var(factor, formula)
@@ -44,6 +45,25 @@ ts_analysis <- function(portfolios, formula, factor=NULL){
 	model_list <- lapply(portfolios, change_dep_var, this_formula)
 	lapply(model_list, lm, data=factors)}
 
+# all_time_series <- lapply(candidate_factors, ts_analysis, portfolios=pfs_25, formula=hml_model)
+
+drop_intercept <- function(data){
+	colnames(data)[1] <- 'intercept'
+	data_2 <- subset(data, select=-c(intercept))
+	return(data_2)
+	}
+
+
+merge_coef <- function(regression_series, factor_names){
+	coef_list <- lapply(regression_series, coef)
+	start <- coef_list[[1]]
+	J=length(coef_list)
+	for(i in 2:J){
+		start <- rbind(start, coef_list[[i]])}
+	return(start)
+	}
 
 
 
+t <- ts_analysis(factor='vc_returns')
+# t_coef <- lapply(t, coef)
